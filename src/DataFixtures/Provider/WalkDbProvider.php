29eb4df52c42843c63534110d7e84975ce14b461
berenice sae <berenice.sae@gmail.com>
2021-03-22 16:46:11 +0100

    data creation with faker

---
+++ src/DataFixtures/Provider/WalkDbProvider.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\DataFixtures\Provider;
+
+class WalkDbProvider
+{
+
+    private $name = [
+        'Bretagne',
+        'Normandie',
+        'Hauts-de-France',
+        'Pays de la Loire',
+        'Île-de-France',
+        'Centre-Val de Loire',
+        'Nouvelles-Aquitaine',
+        'Occitanie',
+        'Auvergne-Rhône-Alpes',
+        'Provence-Alpes-Côte d\'Azur',
+        'Corse',
+        'Grand-Est',
+        'Bourgogne-Franche-Comté',
+        'Mayotte',
+        'Martinique',
+        'Guadeloupe',
+        'Réunion',
+        'Guyane',
+    
+    ];
+
+    private $color = [
+        '#E1645F',
+        '#678E62',
+        '#EDA80A',
+        '#FD827D',
+        '#FDF6F5',
+        '#8C9F75',
+        '#FFCF34',
+        '#FEFAF0',
+    ];
+
+    private $difficulty = [
+        'facile',
+        'moyen',
+        'difficile',
+    ];
+
+    /**
+     * Return a name of area randomly
+     */
+    public function areaName()
+    {
+        return $this->name[array_rand($this->name)];
+    }
+
+    /**
+     * Return a color of area randomly
+     */
+    public function areaColor()
+    {
+        return $this->color[array_rand($this->color)];
+    }
+
+    /**
+     * Return a difficulty of walk randomly
+     */
+    public function walkDifficulty()
+    {
+        return $this->difficulty[array_rand($this->difficulty)];
+    }
+
+
+}
