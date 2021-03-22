501fbbf737eb74ad172d3672966e84cd66594c18
berenice sae <berenice.sae@gmail.com>
2021-03-22 13:01:08 +0100

    Two Entities created: Area and Walk with the relation

---
+++ src/Entity/Area.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AreaRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AreaRepository::class)
+ */
+class Area
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $color;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Walk::class, mappedBy="area")
+     */
+    private $walks;
+
+    public function __construct()
+    {
+        $this->walks = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(string $color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Walk[]
+     */
+    public function getWalks(): Collection
+    {
+        return $this->walks;
+    }
+
+    public function addWalk(Walk $walk): self
+    {
+        if (!$this->walks->contains($walk)) {
+            $this->walks[] = $walk;
+            $walk->setArea($this);
+        }
+
+        return $this;
+    }
+
+    public function removeWalk(Walk $walk): self
+    {
+        if ($this->walks->removeElement($walk)) {
+            // set the owning side to null (unless already changed)
+            if ($walk->getArea() === $this) {
+                $walk->setArea(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Walk.php
@@ -0,0 +1,228 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WalkRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=WalkRepository::class)
+ */
+class Walk
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $startingPoint;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $endPoint;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $duration;
+
+    /**
+     * @ORM\Column(type="string", length=128)
+     */
+    private $difficulty;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $elevation;
+
+    /**
+     * @ORM\Column(type="smallint", nullable=true)
+     */
+    private $maxNbPersons;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updatedAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Area::class, inversedBy="walks")
+     */
+    private $area;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getStartingPoint(): ?string
+    {
+        return $this->startingPoint;
+    }
+
+    public function setStartingPoint(string $startingPoint): self
+    {
+        $this->startingPoint = $startingPoint;
+
+        return $this;
+    }
+
+    public function getEndPoint(): ?string
+    {
+        return $this->endPoint;
+    }
+
+    public function setEndPoint(string $endPoint): self
+    {
+        $this->endPoint = $endPoint;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getDuration(): ?string
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(string $duration): self
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getDifficulty(): ?string
+    {
+        return $this->difficulty;
+    }
+
+    public function setDifficulty(string $difficulty): self
+    {
+        $this->difficulty = $difficulty;
+
+        return $this;
+    }
+
+    public function getElevation(): ?int
+    {
+        return $this->elevation;
+    }
+
+    public function setElevation(?int $elevation): self
+    {
+        $this->elevation = $elevation;
+
+        return $this;
+    }
+
+    public function getMaxNbPersons(): ?int
+    {
+        return $this->maxNbPersons;
+    }
+
+    public function setMaxNbPersons(?int $maxNbPersons): self
+    {
+        $this->maxNbPersons = $maxNbPersons;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getArea(): ?Area
+    {
+        return $this->area;
+    }
+
+    public function setArea(?Area $area): self
+    {
+        $this->area = $area;
+
+        return $this;
+    }
+}
