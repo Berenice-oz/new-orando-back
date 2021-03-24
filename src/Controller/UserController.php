50154b54cf6489db6d151d3249fbc9c5b2865e6e
Pierre-Yves Loukakou <p.loukakou@gmail.com>
2021-03-24 23:41:39 +0100

    send user contact mail with mailer and mailtrap

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserContactType;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,7 +17,7 @@ class UserController extends AbstractController
      * Form to contact a user and send message by mail
      * @Route("/profil/{id<\d+>}/contact-user", name="contact_user", methods={"GET","POST"})
      */
-    public function userContact(Request $request, User $user): Response
+    public function userContact(Request $request, MailerInterface $mailer, User $user = null): Response
     {
         if ($user === null) {
             throw $this->createNotFoundException(
@@ -26,9 +29,26 @@ class UserController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            //Message
+            $message = $form->getData()['message'];
+            //User's email
+            $userEmail =  $user->getEmail();
             //Envoie du mail
+            $email = (new Email())
+            ->from('contact@orando.me')
+            ->to('p.loukakou@gmail.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('You have a new message!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>'. $message .'</p>');
+
+        $mailer->send($email);
             //Flash message
             //Redirection
+            return $this->redirectToRoute('contact_user', ['id' => $user->getId()]);
         }else {
             return $this->render('user/contact.html.twig', [
                 'form' => $form->createView(),
