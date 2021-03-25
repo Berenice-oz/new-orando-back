51509c537e25275fcff90c5ca10f36e1a44fdbbe
Pierre-Yves Loukakou <p.loukakou@gmail.com>
2021-03-25 15:23:08 +0100

    fix migration

--- migrations/Version20210325142201.php
+++ migrations/Version20210325142201.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210325134807 extends AbstractMigration
+final class Version20210325142201 extends AbstractMigration
 {
     public function getDescription() : string
     {
