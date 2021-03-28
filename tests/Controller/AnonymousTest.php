8a1cb9949263ebe2cdab9f71b504c023a8bc8b79
berenice sae <berenice.sae@gmail.com>
2021-03-28 15:53:00 +0200

    last modifications

--- tests/Controller/AnonymousTest.php
+++ tests/Controller/AnonymousTest.php
@@ -12,7 +12,7 @@ class AnonymousTest extends WebTestCase
      *
      * @dataProvider urlProvider
      */
-    public function testRedirectInGet($url): void
+    public function testRedirectGet($url): void
     {
         $client = static::createClient();
         $crawler = $client->request('GET', $url);
@@ -22,6 +22,28 @@ class AnonymousTest extends WebTestCase
     }
 
     public function urlProvider()
+    {
+        yield ['/walk/create'];
+        yield ['/walk/edit/1'];
+        yield ['/profile/1/contact-user'];
+
+    }
+
+    /**
+     * Same test but with POST method
+     *
+     * @dataProvider urlProvider
+     */
+    public function testRedirectPost($url): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('POST', $url);
+
+        // check redirect login page
+        $this->assertResponseStatusCodeSame(302);
+    }
+
+    public function url()
     {
         yield ['/walk/create'];
         yield ['/walk/edit/1'];
