26bf45a1551232d40db993ced85dc6cdd10b8b9f
Pierre-Yves Loukakou <p.loukakou@gmail.com>
2021-03-29 22:19:50 +0200

    unique entity constraint

--- src/Entity/Participant.php
+++ src/Entity/Participant.php
@@ -6,9 +6,11 @@ use Doctrine\ORM\Mapping as ORM;
 use App\Repository\ParticipantRepository;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=ParticipantRepository::class)
+ * @UniqueEntity(fields={"user", "walk"})
  */
 class Participant
 {
