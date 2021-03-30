dcf57709c984461e08602e255d30f079b58f418f
berenice sae <berenice.sae@gmail.com>
2021-03-30 11:35:29 +0200

    walk's data in progress

--- migrations/Version20210330085001.php
+++ migrations/Version20210330085001.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210329233301 extends AbstractMigration
+final class Version20210330085001 extends AbstractMigration
 {
     public function getDescription() : string
     {
---
+++ src/DataFixtures/Data/WalkData.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\DataFixtures\Data;
+
+/**
+ * Data of Walk
+ */
+class WalkData
+{
+    static $tagsData = [
+        ["title" => "", "startingPoint" => "", "endPoint" => "", "date" => "", "duration" => "", "difficulty" => "", "elevation" => "",],
+       
+
+    ];
+}
