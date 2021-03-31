15f1748ba91ff3c8c0b44a6fda6065e298fe5f48
Pierre-Yves Loukakou <p.loukakou@gmail.com>
2021-03-31 12:35:15 +0200

    redirect home  after logout

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -34,4 +34,12 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    /**
+     * @Route("/redirectafterlogout", name="redirectafterlogout")
+     */
+    public function redirectafterlogout()
+    {
+        return $this->redirect('http://localhost:8080');
+    }
 }
