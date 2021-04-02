6272e68518d1d75702ffebb6be13c179d4e4453a
berenice sae <berenice.sae@gmail.com>
2021-04-02 16:35:24 +0200

    add area field on RegisterType

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -2,23 +2,40 @@
 
 namespace App\Form;
 
+use App\Entity\Area;
 use App\Entity\User;
+use App\Repository\AreaRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Validator\Constraints\File;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class RegisterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('area', EntityType::class, [
+                'class' => Area::class,
+                'choice_label' => 'name',
+                'query_builder' => function (AreaRepository $er) {
+                    return $er->createQueryBuilder('a')
+                        ->orderBy('a.name', 'ASC');
+                },
+                'label' => 'Région',
+                'placeholder' => 'Sélectionner votre région...',
+                'constraints' => [
+                    new NotBlank(),
+                ],
+                
+            ])
             ->add('email', EmailType::class, [
                 'label' => 'Votre email:'
             ])
