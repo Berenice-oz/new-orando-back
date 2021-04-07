744d5251c17379e21af3fa0752bc00ef14b1421f
berenice sae <berenice.sae@gmail.com>
2021-04-07 17:24:27 +0200

    resolve problem circular reference on /api/walk/id road

--- src/Controller/Api/WalkController.php
+++ src/Controller/Api/WalkController.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
@@ -60,8 +61,13 @@ class WalkController extends AbstractController
             $walk,
             Response::HTTP_OK,
             [],
-            ['groups' => 'api_walks_read_item'],
-        );
+            ['groups' => 'api_walks_read_item', 
+            
+            ObjectNormalizer::CIRCULAR_REFERENCE_HANDLER => function($object){
+                return $object->getId();
+            }
+            
+            ]);
     }
 
     /**
