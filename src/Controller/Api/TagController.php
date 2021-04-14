3980c6fdff7e5a332549237b3db9fb6cd1d499d9
berenice sae <berenice.sae@gmail.com>
2021-04-14 19:16:34 +0200

    add api_tags road

---
+++ src/Controller/Api/TagController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Repository\TagRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TagController extends AbstractController
+{   
+    /**
+     * Api endpoint to read all tags
+     *
+     * @param TagRepository $tagRepository
+     * @Route("/api/tags", name="api_tags", methods={"GET"})
+     */
+    public function read(TagRepository $tagRepository)
+    {
+        $tags = $tagRepository->findAll();
+
+        return $this->json(
+            $tags, 
+            Response::HTTP_OK, 
+            [],
+            ['groups' => 'api_tags_read']
+        );
+    }
+}
