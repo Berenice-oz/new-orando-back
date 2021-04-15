63a0f18ba725187a71f33eee5904df248e31bc28
berenice sae <berenice.sae@gmail.com>
2021-04-15 12:43:31 +0200

    edit a walk => backoffice

--- src/Controller/Back/TagController.php
+++ src/Controller/Back/TagController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller\Back;
 
+use App\Entity\Tag;
+use App\Form\TagType;
 use App\Repository\TagRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -25,6 +29,85 @@ class TagController extends AbstractController
         ]);
     }
 
+
+    /**
+     * Add a new tag
+     * 
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @return Response|RedirectResponse
+     *
+     * @Route("/back/tag/add", name="tag_add", methods={"GET", "POST"})
+     */
+    public function add(Request $request, EntityManagerInterface $em)
+    {
+        $tag = new Tag();
+
+        $form = $this->createForm(TagType::class, $tag);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $em->persist($tag);
+
+            $em->flush();
+
+            $this->addFlash('success', 'Ajout éffectué avec succès.');
+
+            return $this->redirectToRoute('tag_browse');
+
+
+        }
+
+        return $this->render('back/tag/add.html.twig',[
+            
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Edit a walk
+     * 
+     * @param mixed Tag $tag
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @return Response|RedirectResponse
+     *
+     * @Route("/back/tag/edit/{id<\d+>}", name="tag_edit", methods={"GET", "POST"})
+     */
+    public function edit(Tag $tag = null , Request $request, EntityManagerInterface $em)
+    {
+        
+        if($tag === null){
+
+            throw $this->createNotFoundException('Tag non trouvé.');
+        }
+        
+        $form = $this->createForm(TagType::class, $tag);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $em->flush();
+
+            $this->addFlash('success', 'Modification éffectuée avec succès.');
+
+            return $this->redirectToRoute('tag_browse');
+
+
+        }
+
+        return $this->render('back/tag/edit.html.twig',[
+            'tag' => $tag,
+            'form' => $form->createView(),
+        ]);
+
+
+    }
+
+    
     
 }
 
