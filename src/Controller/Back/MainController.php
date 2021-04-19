670f275512c389a6784cb69b25018f20c49d2c8d
Pikloo <p.loukakou@gmail.com>
2021-04-19 17:23:13 +0200

    home admin done

--- src/Controller/Back/MainController.php
+++ src/Controller/Back/MainController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller\Back;
 
+use App\Repository\TagRepository;
+use App\Repository\UserRepository;
+use App\Repository\WalkRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,8 +17,15 @@ class MainController extends AbstractController
      * 
      * @return Response
      */
-    public function home()
+    public function home(WalkRepository $walkRepository, UserRepository $userRepository, TagRepository $tagRepository)
     {
-        return $this->render('back/home.html.twig');
+        $walks = $walkRepository->findLast();
+        $users = $userRepository->findLast();
+        $tags = $tagRepository->findLast();
+        return $this->render('back/home.html.twig', [
+            'walks' => $walks,
+            'users' => $users,
+            'tags' => $tags
+        ]);
     }
 }
